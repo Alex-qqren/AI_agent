2ba6862a4f12459d6d3f3935e441c7f1c4da2847
Alex-qqren <aleksander.kukurenda@gmail.com>
2026-02-21 15:15:00 +0100

    verbose flag

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
-
-
-
+from google.genai import types
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,16 +19,18 @@ def main():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=args.user_prompt
+        # contents=args.user_prompt
+        contents=messages
     )
 
-    print(f"User prompt: {args.user_prompt}")
-    if response.usage_metadata is None:
-        raise RuntimeError("Token counts are not available in the response.")
-    x = response.usage_metadata.prompt_token_count
-    y = response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {x}")
-    print(f"Response tokens: {y}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        if not response.usage_metadata:
+            raise RuntimeError("Token counts are not available in the response.")
+        x = response.usage_metadata.prompt_token_count
+        y = response.usage_metadata.candidates_token_count
+        print(f"Prompt tokens: {x}")
+        print(f"Response tokens: {y}")
     print("Response:")
     print(response.text)
 
