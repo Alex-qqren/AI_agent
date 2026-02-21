47fdb4edd9096d937455f4e57ab3a388f170b71b
Alex-qqren <aleksander.kukurenda@gmail.com>
2026-02-21 13:14:12 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+
+
+def main():
+    load_dotenv()
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set.")
+
+    question = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=[
+            question
+        ]
+    )
+
+    print(f"User prompt: {question}")
+    if response.usage_metadata is None:
+        raise RuntimeError("Token counts are not available in the response.")
+
+    x = response.usage_metadata.prompt_token_count
+    y = response.usage_metadata.candidates_token_count
+    print(f"Prompt tokens: {x}")
+    print(f"Response tokens: {y}")
+    
+    print("Response:")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
