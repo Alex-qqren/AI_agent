3c870745247802f0855560c60bb83e3ee55a2e6f
Alex-qqren <aleksander.kukurenda@gmail.com>
2026-02-26 12:49:18 +0100

    all LLM function declarations

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 from config import *
+from google.genai import types
 
 def get_file_content(working_directory, file_path):
   try:
@@ -18,3 +19,19 @@ def get_file_content(working_directory, file_path):
       
   except Exception as e:
     return f"Error: {str(e)}"
+  
+  
+schema_get_file_content = types.FunctionDeclaration(
+  name="get_file_content",
+  description="Reads the content of a specified file relative to the working directory, with a maximum character limit to prevent excessively large reads.",
+  parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+      "file_path": types.Schema(
+        type=types.Type.STRING,
+        description="Path of the file to read relative to the working directory."
+      )
+    },
+    required=["file_path"]
+  )
+)
--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from google.genai import types
 
 def run_python_file(working_directory, file_path, args=None):
   try:
@@ -39,3 +40,24 @@ def run_python_file(working_directory, file_path, args=None):
   except Exception as e:
     return f"Error: executing Python file: {e}"
   
+schema_run_python_file = types.FunctionDeclaration(
+  name="run_python_file",
+  description="Executes a specified Python file relative to the working directory with optional command-line arguments, capturing and returning the output and errors.",
+  parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+      "file_path": types.Schema(
+        type=types.Type.STRING,
+        description="Path of the Python file to execute relative to the working directory."
+      ),
+      "args": types.Schema(
+        type=types.Type.ARRAY,
+        items=types.Schema(
+          type=types.Type.STRING
+          ),
+        description="Optional list of command-line arguments to pass to the Python file."
+      )
+    },
+    required=["file_path"]
+  )
+)
