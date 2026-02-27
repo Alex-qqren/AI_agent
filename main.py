e11468e2b262810b9889660b06f4e4768b41db1d
Alex-qqren <aleksander.kukurenda@gmail.com>
2026-02-27 15:32:16 +0100

    calling functions by LLM

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ from google import genai
 from google.genai import types
 
 from prompts import *
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -21,6 +20,13 @@ def main():
         raise RuntimeError("GEMINI_API_KEY environment variable not set.")
 
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    generate_content(client, messages, args.verbose)
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.5-flash',
         contents=messages,
@@ -28,22 +34,29 @@ def main():
             tools=[available_functions], system_instruction=system_prompt
             ),
     )
-
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        if not response.usage_metadata:
-            raise RuntimeError("Token counts are not available in the response.")
-        x = response.usage_metadata.prompt_token_count
-        y = response.usage_metadata.candidates_token_count
-        print(f"Prompt tokens: {x}")
-        print(f"Response tokens: {y}")
-
-    if response.function_calls:
-        for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
-    else:
+    if not response.usage_metadata:
+        raise RuntimeError("Token counts are not available in the response.")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if not response.function_calls:
         print("Response:")
         print(response.text)
+        return 
     
+    function_results = []
+    for function_call in response.function_calls:
+        # print(f"Calling function: {function_call.name}({function_call.args})")
+        function_call_result = call_function(function_call, verbose)
+        if len(function_call_result.parts) == 0:
+            raise Exception("Function call result has no parts.")
+        if function_call_result.parts[0].function_response is None:
+            raise Exception("Function call result part has no function response.")
+        if function_call_result.parts[0].function_response.response is None:
+            raise Exception("Function call result part has no function response content.")
+        function_results.append(function_call_result.parts[0])
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        
 if __name__ == "__main__":
     main()
